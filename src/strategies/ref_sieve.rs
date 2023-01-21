887fecdf16f917a071765099c9fb28c5626dd575
timotree3 <timorcb@gmail.com>
2023-01-20 23:30:31 -0500

    Lock clues

--- src/strategies/ref_sieve.rs
+++ src/strategies/ref_sieve.rs
@@ -209,13 +209,17 @@ fn compare_choice(
         ((Choice::Play(_), _), (Choice::Play(_), _)) => Ordering::Equal,
         ((Choice::Play(_), _), (_, _)) => Ordering::Greater,
         ((_, _), (Choice::Play(_), _)) => Ordering::Less,
+        ((Choice::Discard(_), _), (Choice::Discard(_), _)) => Ordering::Equal,
+        ((Choice::Discard(_), _), _) => Ordering::Greater,
+        (_, (Choice::Discard(_), _)) => Ordering::Less,
         (
             (Choice::Hint(_), ChoiceDesc::Hint(hint_a)),
             (Choice::Hint(_), ChoiceDesc::Hint(hint_b)),
         ) => hint_a.new_plays().cmp(&hint_b.new_plays()),
-        ((Choice::Hint(_), _), _) => Ordering::Greater,
-        (_, (Choice::Hint(_), _)) => Ordering::Less,
-        ((Choice::Discard(_), _), (Choice::Discard(_), _)) => Ordering::Equal,
+        ((Choice::Hint(_), ChoiceDesc::Action(_)), (Choice::Hint(_), _))
+        | ((Choice::Hint(_), _), (Choice::Hint(_), ChoiceDesc::Action(_))) => {
+            panic!("mismatching choice and choicedesc types")
+        }
     }
 }
 
--- src/strategies/ref_sieve/conventions.rs
+++ src/strategies/ref_sieve/conventions.rs
@@ -54,7 +54,18 @@ impl PublicKnowledge {
                     HintCategory::RefPlay(target) => {
                         self.note_mut(target).play = true;
                     }
-                    HintCategory::EightClueStall | HintCategory::FillIn => {}
+                    HintCategory::RefDiscard(target) => {
+                        self.note_mut(target).ptd = true;
+                    }
+                    HintCategory::Lock(player) => {
+                        let newest = *state.hands[player].last().unwrap();
+                        self.note_mut(newest).lock = true;
+                    }
+                    // TODO: stalls should give PTD
+                    HintCategory::EightClueStall
+                    | HintCategory::FillIn
+                    | HintCategory::RankAction
+                    | HintCategory::LockedHandStall => {}
                 }
             }
         }
@@ -91,7 +102,7 @@ impl PublicKnowledge {
     }
 
     fn describe_play(&self, state: &State, card_id: CardId) -> Option<ActionDesc> {
-        if !self.note(card_id).play {
+        if !self.note(card_id).playable() {
             return None;
         }
         // If the next player is not loaded, give them PTD
@@ -154,13 +165,46 @@ impl PublicKnowledge {
             if let Some(target) = self.color_clue_target(state, hint.receiver, &hint.touched) {
                 return Some(HintCategory::RefPlay(target));
             }
+        } else {
+            // A rank clue giving a known play or fixing (with negative info) a bad touched card
+            // does not mean anything extra
+            let fixed_trash = new_known_trash
+                .iter()
+                .any(|&card_id| self.note(card_id).clued);
+            if !new_known_plays.is_empty() || fixed_trash {
+                return Some(HintCategory::RankAction);
+            }
+
+            // TODO: rank clues when there's already PTD
+            if let Some(chop) = self.chop_if_unloaded(state, hint.receiver) {
+                if let Some(target) = self.rank_clue_target(state, hint.receiver, &hint.touched) {
+                    // check if target = chop. if so, it's a lock/8 clue stall
+                    if target == chop {
+                        return if state.board.hints_remaining == state.board.opts.num_hints {
+                            // TODO: 8cs actually should give ptd
+                            Some(HintCategory::EightClueStall)
+                        } else if self.is_locked(state, state.board.player) {
+                            Some(HintCategory::LockedHandStall)
+                        } else {
+                            Some(HintCategory::Lock(hint.receiver))
+                        };
+                    } else {
+                        return Some(HintCategory::RefDiscard(target));
+                    }
+                }
+            } else if state.board.hints_remaining == state.board.opts.num_hints {
+                return Some(HintCategory::EightClueStall);
+            } else if self.is_locked(state, state.board.player) {
+                return Some(HintCategory::LockedHandStall);
+            } else {
+                // TODO: LPC
+                return None;
+            }
         }
 
-        if state.board.hints_remaining == state.board.opts.num_hints {
-            Some(HintCategory::EightClueStall)
-        } else {
-            None
-        }
+        // TODO: loaded stalls
+
+        None
     }
 
     fn color_clue_target(
@@ -169,11 +213,7 @@ impl PublicKnowledge {
         receiver: Player,
         touched: &[CardId],
     ) -> Option<CardId> {
-        let previously_unclued: Vec<CardId> = state.hands[receiver]
-            .iter()
-            .copied()
-            .filter(|&card_id| self.unclued(card_id))
-            .collect();
+        let previously_unclued = self.previously_unclued(state, receiver);
 
         for precedence in 0..previously_unclued.len() {
             let focus = previously_unclued
@@ -188,14 +228,50 @@ impl PublicKnowledge {
         None
     }
 
+    fn rank_clue_target(
+        &self,
+        state: &State,
+        receiver: Player,
+        touched: &[CardId],
+    ) -> Option<CardId> {
+        let previously_unclued: Vec<CardId> = self.previously_unclued(state, receiver);
+
+        for precedence in 0..previously_unclued.len() {
+            let focus = previously_unclued[previously_unclued.len() - precedence - 1];
+            let target = previously_unclued
+                [previously_unclued.len() - ((precedence + 1) % previously_unclued.len()) - 1];
+
+            // TODO: lock clues which touch slot 1
+            if touched.contains(&focus) && !touched.contains(&target) {
+                return Some(target);
+            }
+        }
+
+        None
+    }
+
+    fn previously_unclued(&self, state: &State, receiver: Player) -> Vec<CardId> {
+        state.hands[receiver]
+            .iter()
+            .copied()
+            .filter(|&card_id| self.unclued(card_id))
+            .collect()
+    }
+
     fn chop_if_unloaded(&self, state: &State, player: Player) -> Option<CardId> {
-        (!self.is_loaded(state, player)).then(|| *state.hands[player].last().unwrap())
+        (!self.is_loaded(state, player) && !self.is_locked(state, player))
+            .then(|| *state.hands[player].last().unwrap())
     }
 
     fn is_loaded(&self, state: &State, player: Player) -> bool {
         state.hands[player]
             .iter()
             .any(|&card_id| self.note(card_id).is_action())
+    }
+
+    fn is_locked(&self, state: &State, player: Player) -> bool {
+        let newest = *state.hands[player].last().unwrap();
+        !self.is_loaded(state, player) && self.note(newest).lock
     }
 }
 
@@ -207,9 +283,8 @@ pub fn is_conventional(view: &PlayerView<'_>, desc: &ChoiceDesc) -> bool {
             let card = view.card(*chop);
             // We don't give PTD to criticals or playables if we have a clue
             // TODO: what if we have no choice? Does is_conventional need to be defined relative to alternatives?
-            view.board.hints_remaining == 0 || !view.board.is_playable(card)
-            // TODO: uncomment once we have a conventional way of saving criticals
-            // && view.board.is_dispensable(card)
+            view.board.hints_remaining == 0
+                || (!view.board.is_playable(card) && view.board.is_dispensable(card))
         }
         ChoiceDesc::Action(ActionDesc { gave_ptd: None }) => true,
         ChoiceDesc::Hint(HintDesc {
@@ -220,6 +295,22 @@ pub fn is_conventional(view: &PlayerView<'_>, desc: &ChoiceDesc) -> bool {
     }
 }
 
+fn is_hint_conventional(view: &PlayerView<'_>, hint_category: HintCategory) -> bool {
+    match hint_category {
+        HintCategory::RefPlay(target) => view.board.is_playable(view.card(target)),
+        HintCategory::RefDiscard(target) => {
+            // We don't give PTD to criticals or playables if we have a clue
+            let card = view.card(target);
+            !view.board.is_playable(card) && view.board.is_dispensable(card)
+        }
+        HintCategory::FillIn | HintCategory::RankAction => true,
+        // TODO: a lock should be unconventional if there are cluable safe actions
+        HintCategory::Lock(_) => true,
+        // TODO: stalling should be unconventional if there are cluable safe actions
+        HintCategory::EightClueStall | HintCategory::LockedHandStall => true,
+    }
+}
+
 #[derive(Debug, Copy, Clone, Default)]
 struct Note {
     clued: bool,
@@ -227,6 +318,8 @@ struct Note {
     trash: bool,
     /// Has this card ever been given "permission to discard"?
     ptd: bool,
+    /// If this card is on newest, and the player has no known safe action, then they are locked
+    lock: bool,
 }
 
 impl Note {
@@ -236,6 +329,11 @@ impl Note {
 
     fn unclued(&self) -> bool {
         !self.clued && !self.play && !self.trash
+    }
+
+    fn playable(&self) -> bool {
+        // A card can be playable and later become trash
+        self.play && !self.trash
     }
 }
 
@@ -259,15 +357,26 @@ pub struct HintDesc {
 #[derive(Debug, Clone, Copy)]
 enum HintCategory {
     RefPlay(CardId),
+    RefDiscard(CardId),
+    /// A clue touching new cards means nothing extra if it touches an already clued card and fills it in
     FillIn,
+    /// A rank clue touching new cards means nothing extra if it gives a previously unknown play
+    RankAction,
+    LockedHandStall,
     EightClueStall,
+    Lock(Player),
 }
 
 impl HintCategory {
     fn new_plays(&self) -> usize {
         match self {
             HintCategory::RefPlay(_) => 1,
-            HintCategory::FillIn | HintCategory::EightClueStall => 0,
+            HintCategory::RefDiscard(_)
+            | HintCategory::FillIn
+            | HintCategory::EightClueStall
+            | HintCategory::RankAction
+            | HintCategory::Lock(_)
+            | HintCategory::LockedHandStall => 0,
         }
     }
 }
@@ -275,13 +384,5 @@ impl HintCategory {
 impl HintDesc {
     pub fn new_plays(&self) -> usize {
         self.new_known_plays.len() + self.category.new_plays()
-    }
-}
-
-fn is_hint_conventional(view: &PlayerView<'_>, hint_category: HintCategory) -> bool {
-    match hint_category {
-        HintCategory::RefPlay(target) => view.board.is_playable(view.card(target)),
-        HintCategory::FillIn => true,
-        HintCategory::EightClueStall => true,
     }
 }
